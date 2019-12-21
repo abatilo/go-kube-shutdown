ee87b9b6fc97f69b5110f51f3640bef782b2e304
Aaron <AaronBatilo@gmail.com>
2019-12-21 13:23:18 -0700

    feat: Support setting a timeout with HTTPGet check

--- pkg/ready/checks.go
+++ pkg/ready/checks.go
@@ -30,3 +30,28 @@ func HTTPGet(url string) Check {
 		return nil
 	}
 }
+
+// HTTPGetWithTimeout returns a Check that performs an HTTP GET request against
+// the specified URL. The check fails if the response takes longer than the
+// specified timeout or returns a non-200 status code.
+func HTTPGetWithTimeout(url string, timeout time.Duration) Check {
+	client := http.Client{
+		// Opinionated limit for how slow a server can respond
+		Timeout: timeout,
+		// never follow redirects
+		CheckRedirect: func(*http.Request, []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+	return func() error {
+		resp, err := client.Get(url)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("Received %d", resp.StatusCode)
+		}
+		return nil
+	}
+}
